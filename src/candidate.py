0245d8a832ad6db2d78973632f13adbeae0cee0a
Bram <ogus.bram@gmail.com>
2022-01-15 01:04:52 -0500

    Removed redundant property tags and set up election class

--- src/candidate.py
+++ src/candidate.py
@@ -1,26 +1,11 @@
 class Candidate:
 
-    def __init__(self):
-        self._id = -1
-        self._name = ""
-        self._description = ""
+    def __init__(self, id, name, description):
+        self._id = id
+        self._name = name
+        self._description = description
         self._votes = 0
 
-    @property
-    def id(self):
-        return self._id
-    
-    @id.setter
-    def id(self, value):
-        self._id = value
-    
-    @property
-    def name(self):
-        return self._name
-    
-    @name.setter
-    def name(self, value):
-        self._name = value
     
     @property
     def description(self):
