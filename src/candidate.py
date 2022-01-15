2c6e800405f6a0cc31ec74819d495f23b6e34527
Bram <ogus.bram@gmail.com>
2022-01-15 00:48:59 -0500

    Created Candidate class

---
+++ src/candidate.py
@@ -0,0 +1,42 @@
+class Candidate():
+
+    def __init__(self):
+        self._id = -1
+        self._name = ""
+        self._description = ""
+        self._votes = 0
+
+    @property
+    def id(self):
+        return self._id
+    
+    @id.setter
+    def id(self, value):
+        self._id = value
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        self._name = value
+    
+    @property
+    def description(self):
+        return self._description
+
+    @description.setter
+    def description(self, value):
+        self._description = value
+    
+    @property
+    def votes(self):
+        return self._votes
+
+    @votes.setter
+    def votes(self, value):
+        self._votes = value
+    
+    
+
